e3466dfe75d9ef1ab5ca00926f38546fbbb38ea5
Gaby Cruz <gcruz.a10@gmail.com>
2021-09-14 13:28:59 -0500

    fix navbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -5,6 +5,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,19 +22,39 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AppNavbar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" color="transparent">
-        <Toolbar>
+      <AppBar position="fixed" color="default">
+        <Toolbar variant="dense">
           <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Shop</MenuItem>
+            <MenuItem onClick={handleClose}>Custom Services</MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
             Tasty Crafts
           </Typography>
