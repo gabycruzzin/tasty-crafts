ba111c09ee913bd317b393aad56dee58840d6cc7
Gaby Cruz <gcruz.a10@gmail.com>
2021-09-16 21:56:56 -0500

    update shop page

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,33 +1,21 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import Typography from "@mui/material/Typography";
 
 export function Shop() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
-    </Box>
+    <>
+      <br />
+      <Typography
+        variant="h1"
+        component="div"
+        gutterBottom
+        sx={{ textAlign: "center", padding: "1rem", color: "white" }}
+      >
+        Shop
+      </Typography>
+      <Paper>heyy</Paper>
+    </>
   );
 }
