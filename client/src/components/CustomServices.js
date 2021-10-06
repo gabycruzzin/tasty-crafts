1c2f88a48de08c861ed71a9f1a9cb84bbed6260b
Gaby Cruz <gcruz.a10@gmail.com>
2021-10-05 21:20:05 -0500

    custom gitfts card

--- client/src/components/CustomServices.js
+++ client/src/components/CustomServices.js
@@ -1,3 +1,27 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
 export function CustomServices() {
-  return <>Custom services</>;
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Box
+        sx={{
+          top: "30vh",
+          position: "absolute",
+          textAlign: "center",
+          maxWidth: 300,
+        }}
+      >
+        <Card>
+          <CardContent>
+            <Typography variant="h5">Custom Gifts Coming 2022! 🎉</Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </Grid>
+  );
 }
